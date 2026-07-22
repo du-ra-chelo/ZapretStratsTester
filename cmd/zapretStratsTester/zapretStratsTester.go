291ea7036255e5b95f649fa004a03b85eb59a06a
du-ra-chelo <duRaCHelo@proton.me>
2026-07-23 00:28:46 +0300

    Добавлена проверка на существование передаваемых флагов

--- cmd/zapretStratsTester/zapretStratsTester.go
+++ cmd/zapretStratsTester/zapretStratsTester.go
@@ -1,9 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"log"
+
+	"ZapretStratsTester/internal/osutil"
 )
 
 func main() {
-	fmt.Println(cfg)
+	// Проверяем наличие zapret1 в сиситеме
+	if err := osutil.IsFileExist(cfg.zapretFolder,
+		"Установите zapret или укажите верный путь"); err != nil {
+		log.Fatal(err)
+	}
+	// Проверяем существует ли программа-тестер
+	if err := osutil.IsFileExist(cfg.testerBin,
+		"Перекомпилируйте программу или укажите верный путь"); err != nil {
+		log.Fatal(err)
+	}
+	// Пробуем завершить системный Zapret, если тот запущен
+	if active, err := osutil.IsServiceActive("zapret"); err != nil {
+		log.Fatal(err) // Серивис не существует или непредвиденная ошибка systemctl
+	} else if active {
+		if err := osutil.Systemctl("stop", "zapret"); err != nil { // Пробуем остановить
+			log.Fatal(err)
+		}
+		defer osutil.Systemctl("start", "zapret") // По окончании восстанавливаем состояние
+	}
 }
