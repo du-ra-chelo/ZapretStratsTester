7294a897fbb10a7d90d96b2e90028b3222a1376a
du-ra-chelo <duRaCHelo@proton.me>
2026-07-23 00:28:46 +0300

    добавлены основные флаги запуска

--- cmd/zapretStratsTester/init.go
+++ cmd/zapretStratsTester/init.go
@@ -4,6 +4,13 @@ import (
 	"flag"
 )
 
+const (
+	defaultPathTester = "./tester"
+	defaultPathZapret = "/opt/zapret"
+)
+
+var cfg Config
+
 type Config struct {
 	domainsFile string
 	stratsDir   string
@@ -14,10 +21,18 @@ type Config struct {
 	testerRetries    int
 	testerRetryDelay int
 
+	zapretFolder  string
 	zapretThreads int
 }
 
-var cfg Config
+// Реализация flag.Value
+type stringValue string
+
+func (s *stringValue) String() string { return string(*s) }
+func (s *stringValue) Set(v string) error {
+	*s = stringValue(v)
+	return nil
+}
 
 func init() {
 	domainsFile := flag.String("domains", "./domains.txt", "Список доменов для проверки")
@@ -25,12 +40,17 @@ func init() {
 
 	// Возможность использовать сторонние tester?
 	// На данный момент поддерживается только areAvailable
-	testerBin := flag.String("tester", "./areAvailable", "Путь к программe проверки доступности")
+	var testerBin stringValue = defaultPathTester
+	flag.Var(&testerBin, "tester", "Путь к программe проверки доступности (default "+defaultPathTester+")")
+	flag.Var(&testerBin, "t", "Путь к программe проверки доступности (default "+defaultPathTester+")")
 	testerThreads := flag.Int("tester-threads", 0, "Кол-во одновременных потоков опроса программы проверки. 0 - авто")
 	testerNetTimeout := flag.Int("tester-timeout", 0, "Время ожидания ответа домена. 0 - авто")
 	testerRetries := flag.Int("tester-retries", 0, "Кол-во повторных запросов к домену. 0 - авто")
 	testerRetryDelay := flag.Int("tester-retry-delay", 0, "Задержка между повторными запросами, растет с номером попытки. 0 - авто")
 
+	var zapretFolder stringValue = defaultPathZapret
+	flag.Var(&zapretFolder, "zapret", "Путь к папке zapret (default "+defaultPathZapret+")")
+	flag.Var(&zapretFolder, "z", "Путь к папке zapret (default "+defaultPathZapret+")")
 	zapretThreads := flag.Int("zapret-threads", 3, "Кол-во одновременно запущенных экземпляров zapret")
 	flag.Parse()
 
@@ -38,12 +58,13 @@ func init() {
 		domainsFile: *domainsFile,
 		stratsDir:   *stratsDir,
 
-		testerBin:        *testerBin,
+		testerBin:        testerBin.String(),
 		testerThreads:    *testerThreads,
 		testerNetTimeout: *testerNetTimeout,
 		testerRetries:    *testerRetries,
 		testerRetryDelay: *testerRetryDelay,
 
+		zapretFolder:  zapretFolder.String(),
 		zapretThreads: *zapretThreads,
 	}
 }
