00dd95c64a3551d193add2ea9f9bf2b8365c60bd
du-ra-chelo <duRaCHelo@proton.me>
2026-07-24 00:00:15 +0300

    структура nft правил

--- cmd/main/tablePattern.go
+++ cmd/main/tablePattern.go
@@ -25,4 +25,11 @@ const (
 		meta mark & 0x40000000 != 0x00000000 notrack
 	}
 }`
+	ruleTemplate = `add rule inet zst postnat oifname %s ` + // Правило в таблицу тестирования, интерфейс wan
+		`socket cgroupv2 level 0 "/%s" ` + // Различать по полному пути
+		`meta mark & 0x40000000 == 0x00000000 %s ` + // Перенаправлять только необработанные zapret
+		`ct original packets 1-6 queue num 200 bypass` // первые 6 пакетов достаточно (обычно даже меньше)
+
+	tcp = "tcp dport {80,443}"
+	udp = "udp dport 443"
 )
